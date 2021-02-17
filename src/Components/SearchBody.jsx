f888e201c2ccb4af12368c81b6a8ab3490fdabee
Tanner <tanne@LAPTOP-FO5RENTC>
2021-02-17 05:28:47 -0600

    Reordered Data file and uploading Da Chen's search function without cap sensitivity

--- src/Components/SearchBody.jsx
+++ src/Components/SearchBody.jsx
@@ -5,41 +5,56 @@ import { ProductBody } from "./ProductBody";
 import { Button } from "antd";
 import { HomeBody } from "./HomeBody";
 
+
+
 export class SearchBody extends React.Component {
-    
+
+
+
 
 
     render() {
-        console.log("test5",this.props);
-        if (this.props) {}
+        console.log("test5", this.props);
+        if (this.props) { }
+
+
 
         console.log(this.props)
         const result = Object.keys(this.props.data).filter(key => {
             console.log(key)
-            return this.props.data[key].type === "entry"
+            return this.props.data[key]
         }).filter(key => {
             console.log(key)
-            return this.props.data[key].name.includes(this.props.search)
+            return this.props.data[key].name.toLowerCase().includes(this.props.search.toLowerCase())
         }).map(key => (
+
+
 
             /*<li>
                 <button className="buttons" type="primary" shape="round" onClick={<ProductBody {...key} />}>
                     <Link to={key} >{this.props.data[key].name}</Link>
                 </button>
             </li>*/
+
+
+
             <li>
                 <button className="buttons" type="primary" shape="round" onClick={this.redirect(this.props.data[key])}>
                     <Link to={key} >{this.props.data[key].name}</Link>
                 </button>
             </li>
 
-            ));
+
+
+        ));
         return (
             <>
                 <div className="divide">{result}</div>
             </>
         );
     }
+
+
 
     redirect(key, event) {
         return (<ProductBody {...key} />);
